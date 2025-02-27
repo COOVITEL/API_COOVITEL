27ccb5326cf4690ff5cef080d6f1763e40d3d41a
ManuRodriguezC <estebanclimb@gmail.com>
2025-02-26 21:49:45 -0500

    Update project

--- consultas/views.py
+++ consultas/views.py
@@ -6,7 +6,6 @@ from .services import consult
 from .querys import consultAsociado
 
 @api_view(['GET'])
-# @permission_classes([AllowAny])
 @permission_classes([IsAuthenticated])
 def consulta_asociado(request, cedula=None):
   """ Vista API para la consulta de asociado por el numero de cedula """
@@ -19,17 +18,18 @@ def consulta_asociado(request, cedula=None):
     return Response({'success': False, 'message': 'Cédula no proporcionada'}, status=400)
   
   queryAsociado = consultAsociado(cedula)
-  
+  print(queryAsociado)
+  # return
   data = consult(queryAsociado)
-  
+  print(data)
   if data:
-    setData = {
-      'nombre': f"{data[0]['P_NOMBRE']} {data[0]['S_NOMBRE']} {data[0]['P_APELLIDO']} {data[0]['S_APELLIDO']}",
-      'dirección': data[0]['DIRECCION_CORRES'],
-      'correo': data[0]['EMAIL_CORRES'],
-      'telefono_fijo': data[0]['CELULAR_CORRES'],
-      'telefono_celular': data[0]['CELULAR_CORRES'],
-      'asociado': True
-    }
-    return Response({'success': True, 'data': setData}, status=200)
+    # setData = {
+    #   'nombre': f"{data[0]['P_NOMBRE']} {data[0]['S_NOMBRE']} {data[0]['P_APELLIDO']} {data[0]['S_APELLIDO']}",
+    #   'dirección': data[0]['DIRECCION_CORRES'],
+    #   'correo': data[0]['EMAIL_CORRES'],
+    #   'telefono_fijo': data[0]['CELULAR_CORRES'],
+    #   'telefono_celular': data[0]['CELULAR_CORRES'],
+    #   'asociado': True
+    # }
+    return Response({'success': True, 'data': data}, status=200)
   return Response({'success': False, 'message': 'No se encontro informacion del asociado'}, status=404)
